82194258553bcb8e55cb67ff3295f6581628952b
WeixiSun <wsun293@uwo.ca>
2023-10-28 19:09:29 -0400

    Update insertAll.py
    add scripts which used to allow prof connect with his local database

--- main/insertAll.py
+++ main/insertAll.py
@@ -1,13 +1,27 @@
 import csv
 import psycopg2
 
+dbname_imput = input("Enter database name: ")
+user_imput = input("Enter user name: ")
+password_imput = input("Enter password: ")
+host_imput = input("Enter host: ")
+port_imput = input("Enter port: ")
+
+# conn = psycopg2.connect(
+#     dbname="postgres",
+#     user="postgres",
+#     password="qwer",
+#     host="localhost",
+#     port="5432"
+# )
+# cur = conn.cursor()
 
 conn = psycopg2.connect(
-    dbname="postgres",
-    user="westernuniversity",
-    password="",
-    host="localhost",
-    port="5432"
+    dbname = dbname_imput,
+    user = user_imput,
+    password = password_imput,
+    host = host_imput,
+    port = port_imput
 )
 cur = conn.cursor()
 
